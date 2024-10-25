afaf3ffde439edae90c4706ebc6c21a34411048b
poisonshade <imildositoe@gmail.com>
2024-10-25 23:13:52 +0200

    tables joined from the SQL query

--- clustering.py
+++ clustering.py
@@ -1,25 +1,8 @@
 import pandas as pd
 import sqlite3
 
-df_player = []
-df_player_attributes = []
+conn = sqlite3.connect('database.sqlite')
+df_pp_attributes = pd.read_sql('SELECT * FROM Player p JOIN Player_Attributes pa ON p.player_api_id = pa.player_api_id', conn)
+conn.close()
 
-def get_player():
-    global df_player
-    conn = sqlite3.connect('database.sqlite')
-    cursor = conn.cursor()
-    player = cursor.execute('SELECT * FROM Player')
-    df_player = pd.DataFrame(player)
-    conn.close()
-
-def get_player_attributes():
-    global df_player_attributes
-    conn = sqlite3.connect('database.sqlite')
-    cursor = conn.cursor()
-    player_attributes = cursor.execute('SELECT * FROM Player_Attributes')
-    df_player_attributes = pd.DataFrame(player_attributes)
-    conn.close()
-
-# print(df_player)
-# get_player_attributes()
-print(df_player_attributes)
+print(df_pp_attributes)
